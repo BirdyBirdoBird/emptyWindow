2534a4f6ab88dbf4539c5feb50582e8f12ccf543
davidleshed <davidleshed@gmail.com>
2025-06-18 19:12:30 +0300

    shit works before  I do crazy shit

--- emptyWindow/src/Constants.h
+++ emptyWindow/src/Constants.h
@@ -5,6 +5,69 @@
 namespace Constants {
     constexpr int SCREEN_WIDTH = 800;
     constexpr int SCREEN_HEIGHT = 600;
+    constexpr float VERTICES[] = {
+        // front face
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f,  // bottom right
+        -0.5f, -0.5f,  0.5f,  0.0f, 1.0f,  // bottom left
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,  // top left
+
+        // back face
+         0.5f,  0.5f, -0.5f,  1.0f, 0.0f,  // top right
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,  // bottom right
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
+        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,  // top left
+
+        // left face
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
+        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f,  // bottom right
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
+        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,  // top left
+
+        // right face
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f,  // bottom right
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
+         0.5f,  0.5f, -0.5f,  0.0f, 0.0f,  // top left
+
+         // top face
+          0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
+         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,  // top left
+         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
+          0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // bottom right
+
+          // bottom face
+           0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  // top right
+          -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // top left
+          -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
+           0.5f, -0.5f, -0.5f,  1.0f, 1.0f   // bottom right
+    };
+
+    constexpr unsigned int INDICES[] = {
+        // front face
+        0, 3, 1,
+        1, 3, 2,
+
+        // back face
+        4, 5, 7,
+        5, 6, 7,
+
+        // left face
+        8, 11, 9,
+        9, 11, 10,
+
+        // right face
+        12, 13, 15,
+        13, 14, 15,
+
+        // top face
+        16, 19, 17,
+        17, 19, 18,
+
+        // bottom face
+        20, 21, 23,
+        21, 22, 23
+    };
 }
 
 #endif 
--- emptyWindow/src/Source.cpp
+++ emptyWindow/src/Source.cpp
@@ -11,105 +11,28 @@
 #include <../glm/gtc/type_ptr.hpp>
 #include "Camera.h"
 #include "Constants.h"
+using namespace Constants;
+int initiateGLFW();
+void initiateGLAD();
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void checkKeyboardMovement();
 
+GLFWwindow* window;
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
 bool firstMouse = true;
-float lastX = 800.0f / 2.0;
-float lastY = 600.0 / 2.0;
-
-
+float lastX = SCREEN_WIDTH / 2.0;
+float lastY = SCREEN_HEIGHT / 2.0;
 float deltaTime = 0.0f;	
 float lastFrame = 0.0f;
+
 int main(void)
 {
-    GLFWwindow* window;
-    if (!glfwInit())
-        return -1;
-
-    window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    // GLAD
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    initiateGLFW();
+    initiateGLAD();
 
     Shader ourShader;
-
-    //---FACE
-    float vertices[] = {
-        // front face
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
-         0.5f, -0.5f,  0.5f,  1.0f, 1.0f,  // bottom right
-        -0.5f, -0.5f,  0.5f,  0.0f, 1.0f,  // bottom left
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,  // top left
-
-        // back face
-         0.5f,  0.5f, -0.5f,  1.0f, 0.0f,  // top right
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,  // bottom right
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
-        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,  // top left
-
-        // left face
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
-        -0.5f, -0.5f,  0.5f,  1.0f, 1.0f,  // bottom right
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
-        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,  // top left
-
-        // right face
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
-         0.5f, -0.5f,  0.5f,  1.0f, 1.0f,  // bottom right
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
-         0.5f,  0.5f, -0.5f,  0.0f, 0.0f,  // top left
-
-         // top face
-          0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top right
-         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,  // top left
-         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
-          0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // bottom right
-
-          // bottom face
-           0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  // top right
-          -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // top left
-          -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom left
-           0.5f, -0.5f, -0.5f,  1.0f, 1.0f   // bottom right
-    };
-
-    unsigned int indices[] = {
-        // front face
-        0, 3, 1, 
-        1, 3, 2,
-
-        // back face
-        4, 5, 7,
-        5, 6, 7,
-
-        // left face
-        8, 11, 9,
-        9, 11, 10,
-
-        // right face
-        12, 13, 15,
-        13, 14, 15,
-
-        // top face
-        16, 19, 17,
-        17, 19, 18,
-
-        // bottom face
-        20, 21, 23,
-        21, 22, 23
-    };
 
     unsigned int VBO = 0, VAO = 0, EBO = 0;
     glGenVertexArrays(1, &VAO);
@@ -119,10 +42,10 @@ int main(void)
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, 120 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 120 * sizeof(float), VERTICES, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES), INDICES, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -131,6 +54,8 @@ int main(void)
     // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(2);
+
+    // use front back to choose when to render or not to render faces!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! LATER
 
     //--- TEXTURES!!!
     unsigned int texture;
@@ -160,6 +85,8 @@ int main(void)
 
 
     while (!glfwWindowShouldClose(window)) {
+        checkKeyboardMovement();
+
         float currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
@@ -171,7 +98,7 @@ int main(void)
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(model, 0.0f, glm::vec3(0.5f, 1.0f, 0.0f));
 
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)Constants::SCREEN_WIDTH / (float)Constants::SCREEN_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
         glm::mat4 view = camera.GetViewMatrix();
 
@@ -182,21 +109,8 @@ int main(void)
         int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);  
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
-
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-            camera.ProcessKeyboard(FORWARD, deltaTime);
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-            camera.ProcessKeyboard(BACKWARD, deltaTime);
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-            camera.ProcessKeyboard(LEFT, deltaTime);
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-            camera.ProcessKeyboard(RIGHT, deltaTime);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -208,6 +122,29 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+int initiateGLFW() {
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    return 0;
+}
+void initiateGLAD() {
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -231,4 +168,17 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
     lastX = xpos;
     lastY = ypos;
     camera.ProcessMouseMovement(xoffset, yoffset);
+}
+void checkKeyboardMovement() {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
